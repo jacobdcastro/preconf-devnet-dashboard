b1f2ffcb6f52af680dc928b3c41889443ed3a3eb
Jacob D. Castro <jacob@jdc.dev>
2024-06-11 22:37:02 +0200

    add lookahead data, server scaffold

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
