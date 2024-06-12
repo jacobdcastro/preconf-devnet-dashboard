7eb994eff0b797295c25a32fafd7728e388516f7
Jacob D. Castro <jacob@jdc.dev>
2024-06-12 15:27:53 +0200

    conflict merge

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -7,22 +7,30 @@ import { useQuery } from "@tanstack/react-query";
 import SquareCard from "@/components/ui/squarecard";
 import EpochRow from "./components/EpochRow";
 import { Relayers } from "@/models/preconf";
+import { useEffect } from "react";
 
 const getCurrentRelayer = () => {
   return Relayers[0];
 };
 
 export default function Home() {
-  const { data } = useQuery({
-    queryKey: ["data", { timestamp: Date.now() }],
+  const { data, isLoading, isPending, error } = useQuery({
+    queryKey: ["data"],
     queryFn: async () => {
-      const data = axios({
+      const { data } = await axios({
         method: "GET",
         url: process.env.NEXT_PUBLIC_PRECONF_DASHBOARD_API_BASE_URL + "/data",
       });
       return data;
     },
+    refetchInterval: 400,
+    refetchIntervalInBackground: true,
   });
+
+  useEffect(() => {
+    console.log(data);
+    isPending && console.log("Pending");
+  }, [data, isPending]);
 
   const currentRelayer = getCurrentRelayer();
   return (
